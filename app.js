e3cb15de831e1a179d070b5b2dd7249eab701900
Vincent506 <vicenteneto0806@gmail.com>
2025-08-15 10:42:13 -0300

    adicionadas duas funções

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-function adicionarAmigo(){
-    //testando o repositorio
+let amigos = [];
+
+
+function limpar(){
+    let trash = document.querySelector('input');
+    trash.value = '';
 }
+
+function adicionarAmigo(){
+    let novo = document.querySelector('input').value;    
+    if (novo == null) {
+        alert('Por Favor insira um nome');
+        return;
+    }else{
+        if (amigos.includes(novo)) {
+        alert(`O nome ${novo} ja foi acidionado`);
+        return;
+        
+    }
+    amigos.push(novo);
+    exibirLista();
+    limpar();
+    }
+}
+
+function exibirLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let index = 1; index < amigos.length; index++){
+        const elemento = document.createElement('li');
+        elemento.textContent = amigos[index];
+
+        lista.appendChild(elemento);
+    }
+}
+
+adicionarAmigo();
